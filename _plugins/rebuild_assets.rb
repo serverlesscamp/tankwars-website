6c0b5076e0e9cbd2acf5dbf03d43440233367129
Gojko Adzic <gojko@gojko.com>
2016-08-30 00:50:46 +0200

    speed up build for dev

--- _plugins/rebuild_assets.rb
+++ _plugins/rebuild_assets.rb
@@ -8,6 +8,9 @@ Jekyll::Hooks.register :site, :post_write do |site|
   puts 'generating resources for ' + site.config['asset_version']
   Dir.mkdir(site.config['destination'] + '/v', 0700) #=> 0
   system('./node_modules/.bin/cleancss -o ' + site.config['destination'] + '/v/main-' + site.config['asset_version'] +'.css ' +   site.config['source']  + '/style/main.css')
-  system('./node_modules/.bin/webpack -d ' + site.config['source']  + '/scripts/site.js '  + site.config['destination'] + '/v/dev-' + site.config['asset_version'] + '.js');
-  system('./node_modules/.bin/webpack -p ' + site.config['source']  + '/scripts/site.js '  + site.config['destination'] + '/v/main-' + site.config['asset_version'] + '.js');
+  if (ENV['JEKYLL_ENV'] == 'production') then 
+    system('./node_modules/.bin/webpack -p ' + site.config['source']  + '/scripts/site.js '  + site.config['destination'] + '/v/main-' + site.config['asset_version'] + '.js');
+  else 
+    system('./node_modules/.bin/webpack -d ' + site.config['source']  + '/scripts/site.js '  + site.config['destination'] + '/v/dev-' + site.config['asset_version'] + '.js');
+  end
 end

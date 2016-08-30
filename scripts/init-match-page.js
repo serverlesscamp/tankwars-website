2e0f6f2268d9169c6f78c2a54155096fd7f99c77
Gojko Adzic <gojko@gojko.com>
2016-08-30 23:19:44 +0200

    match page refactor

--- scripts/init-match-page.js
+++ scripts/init-match-page.js
@@ -1,39 +1,54 @@
 /*global module, require */
 
-var promisingXhr = require('./promising-xhr'),
-	TankWarsClientApi = require('./tankwars-client-api'),
-	TankWarsModel = require('./tankwars-model'),
-	mapWidget = require('./tankwars-map-widget');
+var TankWarsModel = require('./tankwars-model'),
+	mapWidget = require('./tankwars-map-widget'),
+	updateTankStatuses = require('./update-tank-statuses'),
+	packOptions = require('./pack-options');
 
-module.exports = function initMatchPage(document, config) {
+module.exports = function initMatchPage(document) {
 	'use strict';
-	var api = new TankWarsClientApi(config.apiEndpoint, promisingXhr),
-		twModel = new TankWarsModel(api),
+	var scaleMultiplier = 30,
 		findElement = function (role) {
 			return document.querySelector('[role=' + role + ']');
 		},
-		initMatch = findElement('initMatch'),
-		log = findElement('log'),
-		matchId = findElement('matchId'),
-		status = findElement('status'),
 		matchContainer = findElement('matchContainer'),
-		matchMap = mapWidget(findElement('matchMap'), 5);
+		matchMap = mapWidget(findElement('matchMap'), scaleMultiplier),
+		initMatch = findElement('initMatch'),
+		status = findElement('status'),
+		log = findElement('log'),
+		model = new TankWarsModel();
 
 
-	initMatch.addEventListener('click', twModel.initiateMatch);
-	twModel.on('initializing', function () {
+	model.on('initializing', function () {
 		status.innerHTML = 'Please wait... initializing';
 	});
-	twModel.on('error', function (err) {
+	model.on('error', function (err) {
 		status.innerHTML = 'Error';
 		log.value = log.value + '\n' + 'ERROR:' + JSON.stringify(err);
 	});
-	twModel.on('newMatch', function (id, map) {
-		matchContainer.classList.add('active');
-		status.innerHTML = 'Match initialized';
-		matchId.innerHTML = id;
-		log.value = log.value + '\n' + JSON.stringify(map);
+
+	model.on('newMatch', function (map) {
 		matchMap.updateMap(map);
+		matchContainer.classList.add('active');
+		updateTankStatuses(map, document);
 	});
+	model.on('change', function (map) {
+		matchMap.updateMap(map);
+		updateTankStatuses(map, document);
+	});
+	initMatch.addEventListener('click', function () {
+		model.newMatch(packOptions(document));
+	});
+	matchMap.addEventListener('click', function (e) {
+		var x, y;
+		if (e.target === this) {
+			x = Math.floor(e.offsetX / scaleMultiplier);
+			y = Math.floor(e.offsetY / scaleMultiplier);
+			model.addWall(x, y);
+		} else if (e.target.tagName === 'WALL') {
+			model.removeWall(parseInt(e.target.getAttribute('x')), parseInt(e.target.getAttribute('y')));
+		}
+	});
+
 };
 

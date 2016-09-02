baf41cbfd089643729e404e84c4d912763fb6fb5
Gojko Adzic <gojko@gojko.com>
2016-09-02 08:39:26 +0100

    triggering from the test page

--- scripts/init-test-page.js
+++ scripts/init-test-page.js
@@ -3,6 +3,8 @@
 var TankWarsModel = require('./tankwars-model'),
 	mapWidget = require('./tankwars-map-widget'),
 	updateTankStatuses = require('./update-tank-statuses'),
+	promisingXhr = require('./promising-xhr'),
+	uuid = require('uuid'),
 	packOptions = require('./pack-options');
 
 module.exports = function initMatchPage(document) {
@@ -31,10 +33,13 @@ module.exports = function initMatchPage(document) {
 		randomMap = findElement('randomMap'),
 		matchContainer = findElement('matchContainer'),
 		matchMap = mapWidget(findElement('matchMap'), scaleMultiplier),
-		model = new TankWarsModel();
+		model = new TankWarsModel(),
+		matchId,
+		log = findElement('log');
 
 	model.on('newMatch', function (map) {
 		matchMap.updateMap(map);
+		matchId = uuid.v4();
 		updateCommandOptions(model.getSupportedCommands());
 		updateTankList(map.tanks);
 		matchContainer.classList.add('active');
@@ -59,6 +64,27 @@ module.exports = function initMatchPage(document) {
 	});
 	findElement('execute').addEventListener('click', function () {
 		model.executeCommand(parseInt(manualTankSelector.value), commandSelector.value);
+	});
+	findElement('executeApi').addEventListener('click', function () {
+		var tankId = parseInt(apiTankSelector.value),
+			toSend = model.getVisibleMapForTank(tankId);
+		toSend.matchId = matchId;
+		log.value = 'sending:\n' + JSON.stringify(toSend, null, 2);
+		promisingXhr({
+			url: findElement('apiUrl').value + '/command',
+			method: 'POST',
+			data: toSend
+		}).then(function (response) {
+			if (response && response.body && response.body.command) {
+				log.value = log.value + '\n-----\n RECEIVED: ' + JSON.stringify(response.body, null, 2);
+				model.executeCommand(tankId, response.body.command);
+			} else {
+				throw 'invalid response: ' + response.body;
+			}
+		}).catch(function (err) {
+			log.value = log.value + '\n-----\n' + err && (err.stack || err.message || JSON.stringify(err));
+		});
+
 	});
 	model.newMatch(packOptions(document));
 };
